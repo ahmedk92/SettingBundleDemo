8bc4591b3f8dca57c5fa46e102dccda3e8bc69ec
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2020-03-14 01:43:59 +0200

    change app language demo

--- SettingBundleDemo/ViewController.swift
+++ SettingBundleDemo/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction private func openSettings(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+    }
 
 
 }
